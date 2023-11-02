8c745599bef38caf675ddf2306b485e5212edb87
Yokk <thanawat.yokk@gmail.com>
2023-11-02 14:21:17 +0700

    add Javadoc to each test

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -7,23 +7,29 @@ import org.junit.jupiter.api.Test;
 public class BoardTest {
 
     /**
-     *  Asserts that a board has created with the grid 1x1.
+     * Test the creation of a Board with a single non-null square.
+     * This test ensures that a Board object is created correctly and that it maintains its invariant.
      */
     @Test
-    void testCreateBoard(){
+    void testCreateBoard() {
         BasicSquare x = new BasicSquare();
         Square[][] grid = {{x}};
         Board board = new Board(grid);
         assertThat(board.invariant()).isTrue();
-        assertThat(board.squareAt(0,0)).isEqualTo(grid[0][0]);
+        assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
     }
 
+    /**
+     * Test the creation of a Board with one non-null and one null square.
+     * This test checks if a Board object can handle a grid containing a null square
+     * and whether it maintains its invariant in such a case.
+     */
     @Test
-    void testCreateBoardWithOneNullSquare(){
+    void testCreateBoardWithOneNullSquare() {
         BasicSquare x = new BasicSquare();
         Square[][] grid = {{x, null}};
         Board board = new Board(grid);
         assertThat(board.invariant()).isFalse();
-        assertThat(board.squareAt(0,1)).isNull();
+        assertThat(board.squareAt(0, 1)).isNull();
     }
 }
